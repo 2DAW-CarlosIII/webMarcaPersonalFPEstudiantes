62552bc73660b48ab128d636a31c0d53f000cc96
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2023-05-05 10:00:37 +0200

    Encaje de estilos

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="/">[LOGO]</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/proyectos">Proyectos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Departamentos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Busca proyectos" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
--- resources/views/proyectos/index.blade.php
+++ resources/views/proyectos/index.blade.php
@@ -1,4 +1,57 @@
 @extends('layouts.master')
 @section('content')
     <h1>TODOS LOS PROYECTOS</h1>
+
+<!--
+    Cardgropups para todos los proyectos, una carta por cada uno
+-->
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card">
+          <a href="/proyectos/show/1" class="text-decoration-none text-reset">
+        <img src="/images/placeholder286x180.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+          </a>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <a href="/proyectos/show/2" class="text-decoration-none text-reset">
+            <img src="/images/placeholder286x180.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+              </a>
+      </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <a href="/proyectos/show/3" class="text-decoration-none text-reset">
+          <img src="/images/placeholder286x180.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+            </a>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <a href="/proyectos/show/4" class="text-decoration-none text-reset">
+              <img src="/images/placeholder286x180.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+                </a>
+        </div>
+      </div>
+  </div>
+
+
 @stop
