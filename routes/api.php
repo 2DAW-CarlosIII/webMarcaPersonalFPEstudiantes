2801824b31f9e0c44f74d4c74218000701b6359a
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2023-06-14 21:52:01 +0200

    Manejo de N:M
    Intento de manejo de la relacion N:M desde el controlador de proyectos

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $user;
  });
 Route::apiResource('users', UserController::class);
+route::post('proyectos/{id}/attach', [ProyectoController::class, 'attachEstudiantes']);
+route::post('proyectos/{id}/detach', [ProyectoController::class, 'detachEstudiantes']);
 Route::apiResource('proyectos', ProyectoController::class);
 
 // emite un nuevo token
